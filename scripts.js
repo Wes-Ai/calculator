a8b456387395463d922bd7a00012d641257d12d4
Wesley <smithw26@wwu.edu>
2022-05-12 14:58:31 -0700

    Calculating logic (add, sub, divide, multiply) added.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 const add = function(a, b) {
     return a + b;
-}
+};
+
+const subtract = function(a, b) {
+	return (a - b);
+};
+
+const multiply = function(arr) {
+    return arr.reduce((prevVal, currentVal) => prevVal * currentVal); 
+};
+
+const divide = function(arr) {
+    return arr.reduce((prevVal, currentVal) => prevVal / currentVal); 
+};
