df6cf7cfb417dd4c3844f8f28974865d97c04989
Wesley <smithw26@wwu.edu>
2022-05-14 21:42:19 -0700

    Step 4 10%: Working on javascipt DOM interactions.

--- scripts.js
+++ scripts.js
@@ -25,3 +25,26 @@ const operate = function(operator, a, b) {
         return "ERROR";
     }
 }
+
+// Create DOM elements
+const zero = document.getElementById("0");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+
+const display = document.getElementById("display");
+// const = document.getElementById("");
+
+
+
+const inputNumber = function(a) {
+    display.textContent = a;
+}
+
+zero.addEventListener('click', () => inputNumber(zero.textContent));
